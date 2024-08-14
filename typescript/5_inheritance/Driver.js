9578313cc17a552285ecf5e0c1aad293cbe33078
shambho55 <rishabhchaturvedi579@gmail.com>
2024-08-14 23:49:33 +0530

    single-inheritance-in-typescript

---
+++ typescript/5_inheritance/Driver.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Shape_1 = require("./Shape");
+var Circle_1 = require("./Circle");
+var Rectangle_1 = require("./Rectangle");
+var myShape = new Shape_1.Shape(9, 15);
+//console.log(myShape.getInfo());
+var myCircle = new Circle_1.Circle(3, 4, 5);
+// console.log(myCircle.getInfo());
+var myRectangle = new Rectangle_1.Rectangle(7, 8, 9, 5);
+// console.log(myRectangle.getInfo());
+var shapeArray = [];
+shapeArray.push(myShape);
+shapeArray.push(myCircle);
+shapeArray.push(myRectangle);
+for (var _i = 0, shapeArray_1 = shapeArray; _i < shapeArray_1.length; _i++) {
+    var itrShape = shapeArray_1[_i];
+    console.log(itrShape.getInfo());
+}
