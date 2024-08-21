ac06b379a74aee0233553928b1af8682bcb0e663
shambho55 <rishabhchaturvedi579@gmail.com>
2024-08-22 00:49:05 +0530

    toDoTable

---
+++ checkOutBooks/frontend/check-out-books-library/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { ExploreTopBooks } from './HomePage/ExploreTopBooks';
+
+function App() {
+  return (
+    <div>
+    <Navbar />
+    <ExploreTopBooks />
+    </div>
+  );
+}
+
+export default App;
--- react-to-do-list/src/components/ToDoTable.tsx
+++ react-to-do-list/src/components/ToDoTable.tsx
@@ -37,5 +37,3 @@ export const ToDoTable : React.FC <{
     )
 
 }
-
-export default ToDoTable;
